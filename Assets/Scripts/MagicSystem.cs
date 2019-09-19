c359154975d40f7a67d9a0112fc77842a7a582f8
May <32718755+PieBug@users.noreply.github.com>
2019-09-18 21:59:07 -0700

    Update MagicSystem.cs
    Added in variables for the shooting system, added a component called LineRenderer which is in place for the raycasting. Currently a work in progress.

--- Assets/Scripts/MagicSystem.cs
+++ Assets/Scripts/MagicSystem.cs
@@ -6,16 +6,28 @@ public class MagicSystem : MonoBehaviour {
     // Start is called before the first frame update
     public GameObject LeftWand;
     public GameObject RightWand;
-    float buttonPressedTime = 10.0f;
+    //float buttonPressedTime = 10.0f;
 
     // Shooting Variables // 
-    public int wandDamage = 1;
-
+    public int wandDamage = 1; // Amount of damage
+    public float fireRate = .25f; // How often player can fire weapon
+    public float shootRange = 50f; // How long rays are shot
+    public float hitForce = 100f; // How much force hitting game objects with a rigidbody
+    public Transform rayEnd; // This will mark the B position where the ray will hit (from point A to point B)
+    private Camera fpsCam;
+    private WaitForSeconds rayDuration = new WaitForSeconds(.07f); // How long ray will remain in game view
+    private AudioSource bulletSound;
+    private LineRenderer laserLine; // takes array of two points and draws a line between each one in the game view
+    private float nextFire; // Holds time when player can fire again after firing
 
     void Start()
     {
         LeftWand = GameObject.Find("LeftWand");
         RightWand = GameObject.Find("RightWand");
+
+        laserLine = GetComponent<LineRenderer>();
+        fpsCam = GetComponentInChildren<Camera>();
+
     }
 
     // Update is called once per frame
@@ -23,32 +35,22 @@ public class MagicSystem : MonoBehaviour {
     {
         if (Input.GetButtonDown("Fire1") && Input.GetButtonDown("Fire2") || Input.GetButtonDown("Fire2") && Input.GetButtonDown("Fire1"))
         {
-            buttonPressedTime = Time.time;
+            //buttonPressedTime = Time.time;
             print("Both wands were pressed!");
 
         }
 
         if (Input.GetButtonDown("Fire1") && !Input.GetButtonDown("Fire2"))
         {
-            buttonPressedTime = Time.time;
+            //buttonPressedTime = Time.time;
             print(LeftWand.name);
-            //shootMagic();
         }
 
         if (Input.GetButtonDown("Fire2") && !Input.GetButtonDown("Fire1"))
         {
-            buttonPressedTime = Time.time;
+            //buttonPressedTime = Time.time;
             print(RightWand.name);
         }
-
-
-        /*
-       void shootMagic(string Magic, int damage)
-       {
-
-       }
-
-       */
 
     }
 }
