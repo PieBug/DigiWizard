d4a674cbd72906b636e4466f179b0d0814b84587
Phischermen <kevfischermen@gmail.com>
2019-10-02 21:33:31 -0700

    Added some pickups
    Added RAM Pickup. Added Health Pickup

---
+++ Assets/PickupResources/Pickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public Collider collider;
+    public GameObject pickup;
+    public ParticleSystem particles;
+    public AudioSource audioSource;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController player = PlayerController.singleton;
+        if (other.gameObject == player.gameObject && CanObtain(player))
+        {
+            Obtain(player);
+        }
+    }
+
+    public virtual bool CanObtain(PlayerController player)
+    {
+        return true;
+    }
+
+    public virtual void Obtain(PlayerController player)
+    {
+        collider.enabled = false;
+        Destroy(pickup);
+        Destroy(gameObject, 10f);
+        particles.Play();
+        audioSource.Play();
+    }
+}
