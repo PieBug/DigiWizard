02fff63d3a952ade18e551d6ee7377a9a6941ad0
RichardCole1098 <richardcole1098@gmail.com>
2019-10-16 18:40:22 -0700

    Zack's Work
    The work Zack did and sent me over google drive

---
+++ Assets/EnemyResources/Scripts/SlugEnemyHealthAndDeathManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlugEnemyHealthAndDeathManager : EnemyHealthAndDeathManager
+{
+    protected override IEnumerator Death()
+    {
+        SlugAI slug = GetComponent<SlugAI>();
+        slug.animator.SetTrigger("death");
+        //slug.attackSphere.gameObject.SetActive(false);
+        slug.state = SlugAI.State.dead;
+        slug.StopAllCoroutines();
+        slug.audioSource2D.PlayOneShot(slug.attributes.deathClip);
+        yield return new WaitForSeconds(1f);
+        //Play particle effect
+        //Play dissappear noise
+        transform.Find("Slug").gameObject.SetActive(false);
+        GetComponent<Collider>().enabled = false;
+        GameObject pickup = Instantiate(slug.attributes.normalDrop, transform.position, Quaternion.identity);
+        pickup.GetComponent<Pickup>().MakeTemporary(3f);
+        Destroy(gameObject, 5f);
+    }
+}
