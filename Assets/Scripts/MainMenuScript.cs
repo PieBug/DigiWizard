690096f7e0c348beb9f6d89c3d8f2d48e9712013
RichardCole1098 <richardcole1098@gmail.com>
2019-10-16 20:45:51 -0700

    Added Trigger that will goto main menu. Cursor unlocks in main menu.

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public void Start(){
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public GameObject playerHUD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == PlayerController.singleton.gameObject){
+            SceneManager.LoadScene(0);
+        }
+    }
+
+   
+}
