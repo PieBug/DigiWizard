5067b2fe5aa11bf630390fa3403a65f0e01d7efc
ZacharyFFuller <50885386+ZacharyFFuller@users.noreply.github.com>
2019-10-16 21:43:34 -0700

    Improved Section 3 of DemoScene
    Improved the box's movemnet
    rearranged the platforms

---
+++ Assets/EnemyResources/Scripts/CubeMove2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove2 : MonoBehaviour
+{
+    private Vector3 pos1 = new Vector3(-12, 5, 130);
+    private Vector3 pos2 = new Vector3(7, 5, 130);
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+    }
+}
---
+++ Assets/EnemyResources/Scripts/CubeMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+
+    private Vector3 pos1 = new Vector3(-12, 5, 108);
+    private Vector3 pos2 = new Vector3(7, 5, 108);
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+    }
+}
+
