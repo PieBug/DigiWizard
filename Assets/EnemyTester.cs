93a17a37df7e869ec31c0d36e1b85fbd3ac31c3c
Phischermen <kevfischermen@gmail.com>
2019-11-04 22:12:24 -0800

    StressTest Enemies
    Added scene to test large number of enemies.

---
+++ Assets/EnemyTester.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTester : MonoBehaviour
+{
+    public Vector2 count;
+    public Vector2 space;
+    public GameObject enemyToTest;
+    public GameObject obstacle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(var i = 0; i < count.x; ++i)
+        {
+            for(var j = 0; j < count.y; ++j)
+            {
+                if((i + j) % 2 == 0)
+                {
+                    GameObject obj = Instantiate(obstacle, new Vector3(i * space.x, 0, j * space.y), Quaternion.identity);
+                    obj.transform.localScale *= (Random.value * 0.9f) + 0.1f;
+                }
+                else
+                {
+                    Instantiate(enemyToTest, new Vector3(i * space.x, 0, j * space.y), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
